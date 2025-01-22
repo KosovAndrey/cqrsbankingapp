119962d2688ad37153e72ac8709220c6f7767bdb
Kosov Andrey <92647577+KosovAndrey@users.noreply.github.com>
2025-01-22 18:47:26 +0300

    Add no-args constructor to exception classes

--- src/main/java/com/kosovandrey/cqrsbankingapp/domain/exception/ResourceAlreadyExistsException.java
+++ src/main/java/com/kosovandrey/cqrsbankingapp/domain/exception/ResourceAlreadyExistsException.java
@@ -1,5 +1,8 @@
 package com.kosovandrey.cqrsbankingapp.domain.exception;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class ResourceAlreadyExistsException extends RuntimeException {
     public ResourceAlreadyExistsException(String message) {
         super(message);
